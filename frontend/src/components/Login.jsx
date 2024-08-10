b2e78dbb4baeba381bbc3d63d801a65557a11a6c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:19:39 +0530

    Imports React: For defining the React component. Input and Button: Custom UI components for input fields and buttons. axios: For making HTTP requests to the backend. toast: From sonner for displaying notifications. Link and useNavigate: From react-router-dom for navigation. Loader2: A spinner icon from lucide-react for indicating loading states. Redux Hooks: useDispatch and useSelector for interacting with the Redux store.

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import axios from 'axios';
+import { toast } from 'sonner';
+import { Link, useNavigate } from 'react-router-dom';
+import { Loader2 } from 'lucide-react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAuthUser } from '@/redux/authSlice';
+
+
+
