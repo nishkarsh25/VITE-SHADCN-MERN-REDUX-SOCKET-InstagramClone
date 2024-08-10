76b7770ca6950f326c506a7e36c46b14153c4c50
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:19:51 +0530

    Component Structure State Management input: Manages the email and password fields. loading: Indicates whether the login process is in progress. user: Accessed from Redux to check if the user is already logged in.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,5 +8,19 @@ import { Loader2 } from 'lucide-react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setAuthUser } from '@/redux/authSlice';
 
+const Login = () => {
+    const [input, setInput] = useState({
+        email: "",
+        password: ""
+    });
+    const [loading, setLoading] = useState(false);
+    const {user} = useSelector(store=>store.auth);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
+    
+
+    
+    return 
+}
 
