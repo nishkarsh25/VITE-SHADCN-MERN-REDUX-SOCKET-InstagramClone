d326952382791a9c534a311c9472003c83207f07
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:15:27 +0530

    Imports Express:
    Imports the Express framework to set up routing.
    Controller Functions:
    
    Imports various controller functions from user.controller.js to handle the business logic for user operations.
    Middleware:
    
    isAuthenticated: Middleware to ensure the user is authenticated before accessing certain routes.
    upload: Middleware for handling file uploads using multer.

---
+++ backend/routes/user.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { editProfile, followOrUnfollow, getProfile, getSuggestedUsers, login, logout, register } from "../controllers/user.controller.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import upload from "../middlewares/multer.js";
+
+
+
+
+
