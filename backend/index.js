bf7687be17e18603001832d349007bd0a750d96f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:19:29 +0530

    Middleware Setup Middleware for Parsing and Cookies:
    express.json(): Middleware to parse JSON bodies.
    cookieParser(): Middleware to parse cookies attached to the client request.
    urlencoded({ extended: true }): Middleware to parse URL-encoded bodies (e.g., from HTML forms).
    CORS Configuration:
    
    cors(corsOptions): Middleware to handle Cross-Origin Resource Sharing (CORS). The corsOptions allow requests from http://localhost:5173 and include credentials (cookies, authorization headers).

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,15 @@ const PORT = process.env.PORT || 3000;
 
 const __dirname = path.resolve();
 
-
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true
+}
+app.use(cors(corsOptions));
 
 
 
