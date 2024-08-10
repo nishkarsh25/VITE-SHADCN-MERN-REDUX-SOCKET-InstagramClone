b5f321a792a5b654b9496c03a42c0c89d14315e8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:29:30 +0530

    Imports React: Required to create the component. Input and Button: Custom components for form input fields and buttons. axios: Used to send HTTP requests to the backend. toast: From sonner to display notifications. Link and useNavigate: From react-router-dom to handle navigation. Loader2: A loading spinner icon from lucide-react. useSelector: A Redux hook to access the global state.

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import axios from 'axios';
+import { toast } from 'sonner';
+import { Link, useNavigate } from 'react-router-dom';
+import { Loader2 } from 'lucide-react';
+import { useSelector } from 'react-redux';
+
+
+
