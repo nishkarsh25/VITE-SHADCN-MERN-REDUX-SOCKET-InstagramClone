ed064d6ca287d1363c14a1348392bf19acea2b89
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:04:23 +0530

    Imports React: Core library needed to build the component. Icons from lucide-react: Various icons used for different sidebar items. Custom components: Components like Avatar, Button, Popover, and CreatePost are custom or UI library components. Utilities: toast from sonner: For showing success or error messages. axios: For making HTTP requests. useNavigate from react-router-dom: For navigating programmatically. useDispatch and useSelector from react-redux: For interacting with the Redux store.

---
+++ frontend/src/components/LeftSidebar.jsx
@@ -0,0 +1,16 @@
+import { Heart, Home, LogOut, MessageCircle, PlusSquare, Search, TrendingUp } from 'lucide-react'
+import React, { useState } from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { toast } from 'sonner'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { setAuthUser } from '@/redux/authSlice'
+import CreatePost from './CreatePost'
+import { setPosts, setSelectedPost } from '@/redux/postSlice'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+import { Button } from './ui/button'
+import { setLikeNotification } from '@/redux/rtnSlice'
+
+
+
