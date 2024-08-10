be5762165f187c9c8613e8e0e0690b978ae49823
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:02:17 +0530

    getAllPost Function Purpose: Retrieves all posts from the database and populates related data. Process: Fetch Posts: Queries the database for all posts and sorts them by creation date in descending order. Populate Author: Populates the author field of each post with username and profile picture. Populate Comments: Populates the comments field of each post with comment details and their authors, sorted by creation date. Response: Sends a response with the list of posts and a success message.

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -45,7 +45,26 @@ export const addNewPost = async (req, res) => {
         console.log(error);
     }
 }
-
+export const getAllPost = async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ createdAt: -1 })
+            .populate({ path: 'author', select: 'username profilePicture' })
+            .populate({
+                path: 'comments',
+                sort: { createdAt: -1 },
+                populate: {
+                    path: 'author',
+                    select: 'username profilePicture'
+                }
+            });
+        return res.status(200).json({
+            posts,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 
 
