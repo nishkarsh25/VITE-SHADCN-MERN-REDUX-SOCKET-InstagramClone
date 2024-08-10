e04855b09c058e26bc497419454a395283a48c68
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:48:57 +0530

    Imports React: For building the component. Redux Hooks: useDispatch, useSelector for interacting with Redux state. Custom UI Components: Avatar, Input, Button for styling and functionality. axios: For making HTTP requests to send messages. lucide-react: MessageCircleCode icon for displaying a default message icon. Custom Components: Messages for displaying the chat history.

---
+++ frontend/src/components/ChatPage.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { setSelectedUser } from '@/redux/authSlice';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { MessageCircleCode } from 'lucide-react';
+import Messages from './Messages';
+import axios from 'axios';
+import { setMessages } from '@/redux/chatSlice';
+
+
+
