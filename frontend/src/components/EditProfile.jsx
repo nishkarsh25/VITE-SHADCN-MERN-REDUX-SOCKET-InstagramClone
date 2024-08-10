0f1b5d3db2153d0e38ac009a31c9f99904e1fdd1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:42:10 +0530

    Imports React: For building the component. Redux: useDispatch and useSelector hooks to manage state. Avatar, AvatarFallback, AvatarImage: Custom components for displaying profile pictures. Button: Custom button component. Textarea: Custom component for multi-line text input. Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue: Custom components for dropdown selection. axios: For making HTTP requests. Loader2: A spinner component for indicating loading state. useNavigate: From react-router-dom for navigation. toast: From sonner for displaying notifications. setAuthUser: Redux action to update user information in the global state.

---
+++ frontend/src/components/EditProfile.jsx
@@ -0,0 +1,14 @@
+import React, { useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { Button } from './ui/button';
+import { Textarea } from './ui/textarea';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from './ui/select';
+import axios from 'axios';
+import { Loader2 } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
+import { setAuthUser } from '@/redux/authSlice';
+
+
+
