bb9528d628c85a4668f3f5f5c03c4e00619d6fa5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:19:56 +0530

    Route Handlers API Routes:
    app.use("/api/v1/user", userRoute): Routes starting with /api/v1/user will be handled by the userRoute router.
    app.use("/api/v1/post", postRoute): Routes starting with /api/v1/post will be handled by the postRoute router.
    app.use("/api/v1/message", messageRoute): Routes starting with /api/v1/message will be handled by the messageRoute router.
    Static File Serving:
    
    app.use(express.static(path.join(__dirname, "/frontend/dist"))): Serves static files from the frontend/dist directory. This is typically where the built frontend assets (HTML, CSS, JavaScript) are located.
    Catch-All Route:
    
    app.get("*", (req, res) => { ... }): Serves the main HTML file for all unmatched routes. This is useful for single-page applications (SPAs) where the frontend handles routing on the client side.

--- backend/index.js
+++ backend/index.js
@@ -26,9 +26,15 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
+// yha pr apni api ayengi
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
 
 
-
-
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
 
 
