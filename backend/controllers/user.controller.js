2e88f565dd8e972dd9adfd8a702c090d2f588104
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 17:59:36 +0530

    login Function Purpose: Handles user login by verifying credentials and issuing a JWT token. Process: Extract Data: Extracts email and password from the request body. Check for Missing Fields: Returns a 401 response if either email or password is missing. Find User: Queries the database for a user with the provided email. Check Credentials: Uses bcrypt to compare the provided password with the stored hashed password. If incorrect, returns a 401 response. Generate Token: Issues a JWT token with the user's ID, which expires in one day. Populate Posts: Retrieves and filters posts authored by the user. Response: Sets the token as an HTTP-only cookie and returns a 200 response with the user's information and a welcome message.

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -34,7 +34,62 @@ export const register = async (req, res) => {
         console.log(error);
     }
 }
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(401).json({
+                message: "Something is missing, please check!",
+                success: false,
+            });
+        }
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({
+                message: "Incorrect email or password",
+                success: false,
+            });
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(401).json({
+                message: "Incorrect email or password",
+                success: false,
+            });
+        };
 
+        const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1d' });
+
+        // populate each post if in the posts array
+        const populatedPosts = await Promise.all(
+            user.posts.map( async (postId) => {
+                const post = await Post.findById(postId);
+                if(post.author.equals(user._id)){
+                    return post;
+                }
+                return null;
+            })
+        )
+        user = {
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            profilePicture: user.profilePicture,
+            bio: user.bio,
+            followers: user.followers,
+            following: user.following,
+            posts: populatedPosts
+        }
+        return res.cookie('token', token, { httpOnly: true, sameSite: 'strict', maxAge: 1 * 24 * 60 * 60 * 1000 }).json({
+            message: `Welcome back ${user.username}`,
+            success: true,
+            user
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 
 
