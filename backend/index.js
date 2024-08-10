7dfb3698d027017d16586ef32c895919b55fd817
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:19:05 +0530

    Imports Express and Middleware:
    express: The main Express framework to handle HTTP requests.
    urlencoded: Middleware for parsing URL-encoded bodies (from HTML forms).
    cors: Middleware for enabling Cross-Origin Resource Sharing (CORS).
    cookieParser: Middleware for parsing cookies.
    dotenv: Loads environment variables from a .env file into process.env.
    Utility Functions and Files:
    
    connectDB: Function to connect to the MongoDB database.
    userRoute, postRoute, messageRoute: Route handlers for different parts of the application (user, post, and message functionalities).
    Socket.io Setup:
    
    app, server from ./socket/socket.js: The Express application and HTTP server integrated with Socket.io.
    Path Module:
    
    path: Provides utilities for working with file and directory paths.

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,25 @@
-import express from "express"
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
