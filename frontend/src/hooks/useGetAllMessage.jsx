42ae64fffbfecfabdca8ec1bd0f62b8862555063
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:52:04 +0530

    Imports setMessages:
    An action creator from the chatSlice Redux slice. This action updates the messages state in the Redux store.
    setPosts:
    
    An action creator from the postSlice Redux slice. Although it’s imported, it is not used in this specific hook.
    axios:
    
    A popular HTTP client for making requests to external APIs. It is used here to fetch data from the backend.
    useEffect:
    
    A React hook that performs side effects in function components. It’s used to trigger the fetch operation when dependencies change.
    useDispatch:
    
    A React-Redux hook that returns the dispatch function, allowing you to dispatch actions to the Redux store.
    useSelector:
    
    A React-Redux hook that allows you to extract data from the Redux store state, using a selector function.

---
+++ frontend/src/hooks/useGetAllMessage.jsx
@@ -0,0 +1,7 @@
+import { setMessages } from "@/redux/chatSlice";
+import { setPosts } from "@/redux/postSlice";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+
