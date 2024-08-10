8ede5c1b31f6637f671385812432a6d7c9b66da3
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:16:59 +0530

    Imports React: Required for creating the React component. Avatar Components: Used for displaying user profile pictures. Redux Hooks: useSelector to access the Redux store's state. React Router DOM: Link for navigation to different routes within the application. SuggestedUsers: A custom component that presumably displays a list of suggested users.

---
+++ frontend/src/components/RightSidebar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
+import SuggestedUsers from './SuggestedUsers';
+
+
+
