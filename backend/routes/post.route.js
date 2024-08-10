31935f10d64b842a63ca520a373e604a596b31e5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:16:22 +0530

    Imports Express:
    Imports the Express framework to set up routing.
    Middleware:
    
    isAuthenticated: Middleware to ensure the user is authenticated before accessing certain routes.
    upload: Middleware for handling file uploads using multer.
    Controller Functions:
    
    Imports various controller functions from post.controller.js to handle the business logic for post operations.

---
+++ backend/routes/post.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import upload from "../middlewares/multer.js";
+import { addComment, addNewPost, bookmarkPost, deletePost, dislikePost, getAllPost, getCommentsOfPost, getUserPost, likePost } from "../controllers/post.controller.js";
+
+
+
+
+
+
+
