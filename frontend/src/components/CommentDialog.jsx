1460c8bb24eb4e306ad69f3b09460ad332f4b895
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:46:29 +0530

    Imports React: For building the component. Dialog, DialogContent, DialogTrigger: Custom components for modal dialogs. Avatar, AvatarFallback, AvatarImage: Custom components for displaying user profile pictures. Link: For navigation. MoreHorizontal: Icon for additional actions. Button: Custom button component. useDispatch, useSelector: Hooks from Redux for managing and accessing state. Comment: A component to render individual comments. axios: For making HTTP requests. toast: For displaying notifications.

---
+++ frontend/src/components/CommentDialog.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { Link } from 'react-router-dom'
+import { MoreHorizontal } from 'lucide-react'
+import { Button } from './ui/button'
+import { useDispatch, useSelector } from 'react-redux'
+import Comment from './Comment'
+import axios from 'axios'
+import { toast } from 'sonner'
+import { setPosts } from '@/redux/postSlice'
+
+
+
