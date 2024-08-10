79ed430c7446a5632097c0a959fd5e6912f177ad
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:17:21 +0530

    Imports Express:
    Imports the Express framework to handle HTTP requests and route them to appropriate handlers.
    Middleware:
    
    isAuthenticated: Middleware that ensures the user is authenticated before accessing the message routes.
    Controller Functions:
    
    getMessage: Controller function to handle retrieving messages for a conversation.
    sendMessage: Controller function to handle sending a new message.

---
+++ backend/routes/message.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+ import isAuthenticated from "../middlewares/isAuthenticated.js";
+import upload from "../middlewares/multer.js";
+import { getMessage, sendMessage } from "../controllers/message.controller.js";
+
+
+
+
+ 
