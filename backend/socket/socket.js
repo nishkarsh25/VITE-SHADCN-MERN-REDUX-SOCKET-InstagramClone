fa3c3dcb105abf5dfc516c4d309170f116ae2e01
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:14:14 +0530

    Imports and Initialization Import Dependencies:
    Server from socket.io: Provides the Socket.IO server functionality.
    express: A popular web framework for Node.js.
    http: A module to create an HTTP server.
    Initialize Express App:
    
    Creates an Express application instance (app).
    Create HTTP Server:
    
    Wraps the Express app with an HTTP server instance (server).
    Initialize Socket.IO:
    
    Creates a new Socket.IO server instance (io), attaching it to the HTTP server.
    Configures Socket.IO with CORS settings to allow connections only from a specified origin (http://localhost:5173) and allows GET and POST methods.

---
+++ backend/socket/socket.js
@@ -0,0 +1,20 @@
+import {Server} from "socket.io";
+import express from "express";
+import http from "http";
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin:'http://localhost:5173',
+        methods:['GET','POST']
+    }
+})
+
+
+
+
+
+
