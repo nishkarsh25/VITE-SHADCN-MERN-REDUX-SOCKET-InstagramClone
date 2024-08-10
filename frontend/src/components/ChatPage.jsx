cb1bdce5b72232945bb9115fa9c394fb5ba13aae
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:49:04 +0530

    State and Props textMessage: State to store the current text input from the user. suggestedUsers: List of users suggested for chat, obtained from the Redux store. selectedUser: The user currently selected for chatting. onlineUsers: List of users who are currently online. messages: List of messages in the current chat, managed by the Redux store.

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -9,5 +9,16 @@ import Messages from './Messages';
 import axios from 'axios';
 import { setMessages } from '@/redux/chatSlice';
 
+const ChatPage = () => {
+    const [textMessage, setTextMessage] = useState("");
+    const { user, suggestedUsers, selectedUser } = useSelector(store => store.auth);
+    const { onlineUsers, messages } = useSelector(store => store.chat);
+    const dispatch = useDispatch();
 
+    
+
+    
+
+    return 
+}
 
