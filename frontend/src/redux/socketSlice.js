ed7d7279034b5e074d72bc7ff1cf4e52455739c5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:45:36 +0530

    socketSlice Definition name: Specifies the name of the slice. In this case, it's "socketio", indicating that this slice manages the state related to WebSocket or socket connections.
    initialState: Sets the initial state for this slice. It contains:
    
    socket: Initially set to null. This is where the WebSocket or socket instance will be stored once it's established.
    reducers: Defines the function(s) to handle state updates:
    
    setSocket: A reducer function that updates the socket state with the data provided in the action payload. The payload is expected to be the WebSocket or socket instance.

--- frontend/src/redux/socketSlice.js
+++ frontend/src/redux/socketSlice.js
@@ -1,3 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
+const socketSlice = createSlice({
+    name:"socketio",
+    initialState:{
+        socket:null
+    },
+    reducers:{
+        // actions
+        setSocket:(state,action) => {
+            state.socket = action.payload;
+        }
+    }
+});
