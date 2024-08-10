5abfb5d4b4c6b47671c2722e07cece9d93b954e9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:50:10 +0530

    Component Breakdown Imports React: For creating the component. Custom UI Components: Avatar, Button from ./ui/avatar and ./ui/button for styling and functionality. Link: From react-router-dom for navigation. Redux Hooks: useSelector for accessing the Redux store. Custom Hooks: useGetAllMessage and useGetRTM for fetching messages and handling real-time updates.

---
+++ frontend/src/components/Messages.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { Button } from './ui/button'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import useGetAllMessage from '@/hooks/useGetAllMessage'
+import useGetRTM from '@/hooks/useGetRTM'
+
+
+
