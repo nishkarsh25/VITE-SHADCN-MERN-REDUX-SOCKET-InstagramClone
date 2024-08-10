ee3923e7f7903e131d902df6124916596e143dbc
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:15:07 +0530

    Imports React: Required to use React features. Avatar Components: For displaying user profile pictures. Dialog Components: For displaying a modal dialog with additional post options. Icons: Various icons from lucide-react and react-icons for UI interactions. CommentDialog: A custom component for handling comments on posts. Redux Hooks: useDispatch and useSelector to interact with the Redux store. Axios: For making HTTP requests. Toast Notifications: For displaying success and error messages.

---
+++ frontend/src/components/Post.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog'
+import { Bookmark, MessageCircle, MoreHorizontal, Send } from 'lucide-react'
+import { Button } from './ui/button'
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import CommentDialog from './CommentDialog'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+import { toast } from 'sonner'
+import { setPosts, setSelectedPost } from '@/redux/postSlice'
+import { Badge } from './ui/badge'
+
+
+
