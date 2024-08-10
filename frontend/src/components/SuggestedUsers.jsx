43f9e0e0f4bd14b891ef887864d511265a244c38
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:18:12 +0530

    Imports React: Required for defining the React functional component. Redux Hook: useSelector is used to access the Redux store's state. React Router DOM: Link is used to navigate to different routes within the application. Avatar Components: Avatar, AvatarFallback, and AvatarImage are used for displaying user profile pictures.

---
+++ frontend/src/components/SuggestedUsers.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+
+
+
