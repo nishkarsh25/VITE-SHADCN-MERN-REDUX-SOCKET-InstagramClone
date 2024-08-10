6e67bbad680b8b149894e150031b11393f02ca82
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:40:43 +0530

    Component Structure State and Hooks params: Extracts route parameters using useParams, specifically the id parameter, which represents the user ID. userId: The ID of the user whose profile is being viewed. useGetUserProfile(userId): Custom hook to fetch the user profile data based on userId. activeTab: Local state to track the currently active tab ('posts' or 'saved'). userProfile, user: Retrieved from the Redux store. userProfile contains data about the user whose profile is being displayed, and user contains the currently logged-in user’s data.

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -7,5 +7,18 @@ import { Button } from './ui/button';
 import { Badge } from './ui/badge';
 import { AtSign, Heart, MessageCircle } from 'lucide-react';
 
+const Profile = () => {
+  const params = useParams();
+  const userId = params.id;
+  useGetUserProfile(userId);
+  const [activeTab, setActiveTab] = useState('posts');
 
+  const { userProfile, user } = useSelector(store => store.auth);
+
+  
+
+  
+
+  return 
+}
 
