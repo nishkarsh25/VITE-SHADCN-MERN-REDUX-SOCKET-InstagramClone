27c20c767e06c686cc2a6706c7f37e449373ebff
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 21:05:08 +0530

    Imports React: Required for creating the component. Various Components: Includes different pages like ChatPage, EditProfile, Home, etc., and layout components like MainLayout. Routing: Uses createBrowserRouter and RouterProvider from react-router-dom to manage routes. Socket.IO: For real-time communication (io). Redux: For state management (useDispatch, useSelector). Socket and Notification Actions: Actions to set up and manage Socket.IO and notifications in the Redux store. ProtectedRoutes: For protecting routes that require authentication.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react'
+import ChatPage from './components/ChatPage'
+import EditProfile from './components/EditProfile'
+import Home from './components/Home'
+import Login from './components/Login'
+import MainLayout from './components/MainLayout'
+import Profile from './components/Profile'
+import Signup from './components/Signup'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { io } from "socket.io-client";
+import { useDispatch, useSelector } from 'react-redux'
+import { setSocket } from './redux/socketSlice'
+import { setOnlineUsers } from './redux/chatSlice'
+import { setLikeNotification } from './redux/rtnSlice'
+import ProtectedRoutes from './components/ProtectedRoutes'
+
+
+
+
+
+
+
