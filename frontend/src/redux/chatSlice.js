5170717151f6f8adf11b9703303a6f1e5bfa06b8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:44:36 +0530

    chatSlice Definition name: Specifies the name of the slice. In this case, it's "chat", indicating that this slice manages chat-related state.
    initialState: Sets the initial state for this slice. It contains:
    
    onlineUsers: An empty array that will hold the list of users currently online.
    messages: An empty array that will store the messages exchanged in the chat.
    reducers: Defines the functions to handle state updates:
    
    setOnlineUsers: A reducer function that updates the onlineUsers state with the data provided in the action payload. The payload is expected to be an array of users who are currently online.
    setMessages: A reducer function that updates the messages state with the data provided in the action payload. The payload is expected to be an array of messages.

--- frontend/src/redux/chatSlice.js
+++ frontend/src/redux/chatSlice.js
@@ -1,3 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
+const chatSlice = createSlice({
+    name:"chat",
+    initialState:{
+        onlineUsers:[],
+        messages:[],
+    },
+    reducers:{
+        // actions
+        setOnlineUsers:(state,action) => {
+            state.onlineUsers = action.payload;
+        },
+        setMessages:(state,action) => {
+            state.messages = action.payload;
+        }
+    }
+});
