892c308ffc28ccb4107679089b6ddfe0150e5254
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 19:15:16 +0530

    Component State and Hooks State Variables:
    text: Tracks the content of the comment input field.
    open: Determines whether the comment dialog is open.
    liked: Boolean indicating if the post is liked by the current user.
    postLike: The number of likes on the post.
    comment: Array of comments associated with the post.
    Redux Hooks:
    
    useSelector: Used to access the Redux store's state, specifically the user and posts data.
    useDispatch: Used to dispatch actions to the Redux store.

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -11,5 +11,17 @@ import { toast } from 'sonner'
 import { setPosts, setSelectedPost } from '@/redux/postSlice'
 import { Badge } from './ui/badge'
 
+const Post = ({ post }) => {
+    const [text, setText] = useState("");
+    const [open, setOpen] = useState(false);
+    const { user } = useSelector(store => store.auth);
+    const { posts } = useSelector(store => store.post);
+    const [liked, setLiked] = useState(post.likes.includes(user?._id) || false);
+    const [postLike, setPostLike] = useState(post.likes.length);
+    const [comment, setComment] = useState(post.comments);
+    const dispatch = useDispatch();
 
+    
+    return
+}
 
