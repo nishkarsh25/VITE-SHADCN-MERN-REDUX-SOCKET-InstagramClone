6419e7b7b10a1acfa5b569b1efed600b4e71ac82
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:16:32 +0530

    Router Initialization Initialize Router: Creates an instance of an Express router (router), which will be used to define routes and their associated handlers.

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -3,7 +3,7 @@ import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../middlewares/multer.js";
 import { addComment, addNewPost, bookmarkPost, deletePost, dislikePost, getAllPost, getCommentsOfPost, getUserPost, likePost } from "../controllers/post.controller.js";
 
-
+const router = express.Router();
 
 
 
