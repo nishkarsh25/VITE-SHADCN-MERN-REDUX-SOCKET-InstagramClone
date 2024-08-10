1f11d291269ab2d2e8688c27c020b59bfa07f8d1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-10 18:47:24 +0530

    Imports combineReducers: A Redux utility to combine multiple reducers into a single reducer function. configureStore: A Redux Toolkit function that simplifies store setup with pre-configured defaults. persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER: Utilities and action types from redux-persist to manage the persistence of the Redux state. storage: The default storage engine from redux-persist, which stores state in local storage.

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,26 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice.js";
+import postSlice from './postSlice.js';
+import socketSlice from "./socketSlice.js"
+import chatSlice from "./chatSlice.js";
+import rtnSlice from "./rtnSlice.js";
+
+import { 
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+
+
+
+
+
+
+
